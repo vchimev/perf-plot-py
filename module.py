7ad8f369ae7cca5b347fcead4531178f73def6b4
Vasil Chimev <Vasil.Chimev@telerik.com>
2017-05-14 18:28:23 +0300

    Improvements

--- module.py
+++ module.py
@@ -1,8 +1,17 @@
+# pylint: disable=C0103
+"""
+    This module:
+        - reads a .csv file
+        - creates a plot
+        - saves it to a .png file
+"""
 import pandas as pd
 
 df = pd.read_csv('perf_info.csv')
-data = df['Launch'].tail(15) # Returns last n rows
-plot = data.plot(title='Start Time')
+data = df[['Timestamp', 'Launch']].loc[df['Hostname']
+                                       == 'mcsofnsbuild07'].tail(15)
+plot = data.plot(x='Timestamp', y='Launch', legend=False,
+                 linewidth=5, rot=23, title='Start Time')
 fig = plot.get_figure()
+fig.set_size_inches(10, 8)
 fig.savefig('perf_info.png')
-
